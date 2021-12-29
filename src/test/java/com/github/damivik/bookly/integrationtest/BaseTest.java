679c3c1c1d7548c19dfe6ae8b1f30d7744e08b53
Dami Oyediran <oyediran.viktor@gmail.com>
2021-12-29 17:58:03 +0100

    Remove unused imports

--- src/test/java/com/github/damivik/bookly/integrationtest/BaseTest.java
+++ src/test/java/com/github/damivik/bookly/integrationtest/BaseTest.java
@@ -3,12 +3,8 @@ package com.github.damivik.bookly.integrationtest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.web.servlet.MockMvc;
 
 @Tag("integration-test")
