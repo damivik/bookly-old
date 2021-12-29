284f7c470f86416529c74b09eb774a849c5fa1fe
Dami Oyediran <oyediran.viktor@gmail.com>
2021-12-29 17:59:21 +0100

    Set security configurations for only api routes

--- src/main/java/com/github/damivik/bookly/SecurityConfiguration.java
+++ src/main/java/com/github/damivik/bookly/SecurityConfiguration.java
@@ -1,50 +1,41 @@
 package com.github.damivik.bookly;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
-	}
-	
-	@Autowired
-	private UserDetailsService userDetailsService;
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.httpBasic()
-			.and()
-			.csrf()
-			.disable()
-			.authorizeRequests()
-			.antMatchers(HttpMethod.POST, "/api/users")
-			.permitAll()
-			.antMatchers("/api/**")
-			.hasRole("USER")
-			.and()
-			.headers()
-			.frameOptions()
-			.sameOrigin();	
-	}
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-			.userDetailsService(userDetailsService)
-			.passwordEncoder(passwordEncoder());
+	}	
+
+	@Configuration
+	@Order(1)                                                        
+	public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+				.antMatcher("/**")
+				.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/api/users")
+				.permitAll()
+				.antMatchers("/api/**")
+				.hasRole("USER")
+				
+				.and()
+				.httpBasic()
+				
+				.and()
+				.csrf()
+				.disable();
+		}
 	}
 }
